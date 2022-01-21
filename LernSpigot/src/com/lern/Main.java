5d0b66f8686a0a32b157174672d89859585eaef8
BuildTools <unconfigured@null.spigotmc.org>
2022-01-21 19:56:26 +0700

    Main update//Log update

--- LernSpigot/src/com/lern/Main.java
+++ LernSpigot/src/com/lern/Main.java
@@ -10,10 +10,10 @@ import net.md_5.bungee.api.ChatColor;
 public class Main extends JavaPlugin{
 	@Override
 	public void onEnable() {
-		getServer().getLogger().info(ChatColor.GREEN + "==========[Plugin]==========");
+		getServer().getLogger().info(ChatColor.GREEN + "===============[Plugin]===============");
 		getServer().getLogger().info(ChatColor.GREEN + "- Lern Plugin Id Enable!");
-		getServer().getLogger().info(ChatColor.GREEN + "- " + Bukkit.getVersion());
-		getServer().getLogger().info(ChatColor.GREEN + "============================");
+		getServer().getLogger().info(ChatColor.GREEN + "- Version: " + Bukkit.getVersion());
+		getServer().getLogger().info(ChatColor.GREEN + "======================================");
 		getServer().getPluginManager().registerEvents(new ItemFrameHide(), this);
 	}
 }
