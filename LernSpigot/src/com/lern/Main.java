85dc7689b165808f9dcb0504be792d7bce7572fa
BuildTools <unconfigured@null.spigotmc.org>
2022-01-21 18:10:16 +0700

    4

--- LernSpigot/src/com/lern/Main.java
+++ LernSpigot/src/com/lern/Main.java
@@ -2,10 +2,11 @@ package com.lern;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Main extends JavaPlugin{
 	@Override
 	public void onEnable() {
-		getServer().getLogger().info(ChatColor.GREEN ×
-					"");
+		getServer().getLogger().info(ChatColor.GREEN + "");
 	}
 }
