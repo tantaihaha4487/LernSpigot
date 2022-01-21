3a941113f3de6704364a4093acabda78e12b7d5d
TantaiHaha <78604116+tantaihaha4487@users.noreply.github.com>
2022-01-21 18:13:57 +0700

    5
    5

--- LernSpigot/src/com/lern/Main.java
+++ LernSpigot/src/com/lern/Main.java
@@ -7,6 +7,8 @@ import net.md_5.bungee.api.ChatColor;
 public class Main extends JavaPlugin{
 	@Override
 	public void onEnable() {
-		getServer().getLogger().info(ChatColor.GREEN + "");
+		getServer().getLogger().info(ChatColor.GREEN + "==========[Plugin]==========");
+		getServer().getLogger().info(ChatColor.GREEN + "- Lern Plugin Id Enable!");
+		getServer().getLogger().info(ChatColor.GREEN + "============================")
 	}
 }
