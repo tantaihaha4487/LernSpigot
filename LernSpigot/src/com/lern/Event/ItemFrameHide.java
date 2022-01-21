ad888ac009e56263da9c38ce2b007de792e24f6a
BuildTools <unconfigured@null.spigotmc.org>
2022-01-21 19:54:31 +0700

    Item frame update

--- LernSpigot/src/com/lern/Event/ItemFrameHide.java
+++ LernSpigot/src/com/lern/Event/ItemFrameHide.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 
