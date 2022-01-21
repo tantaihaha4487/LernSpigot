6457766ecbbf9550d35abf4e436b280565914f66
BuildTools <unconfigured@null.spigotmc.org>
2022-01-21 22:50:37 +0700

    add Glowing Item Frame to hide

--- LernSpigot/src/com/lern/Event/ItemFrameHide.java
+++ LernSpigot/src/com/lern/Event/ItemFrameHide.java
@@ -3,6 +3,7 @@ package com.lern.Event;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.GlowItemFrame;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,19 +18,24 @@ public class ItemFrameHide implements Listener{
 	public void OnCick(PlayerInteractAtEntityEvent e) {
 		Player p = e.getPlayer();
 		ItemFrame item = (ItemFrame) e.getRightClicked();
+		GlowItemFrame gitem = (GlowItemFrame) e.getRightClicked();
 		String msg = "" + ChatColor.GOLD + ChatColor.BOLD + "(" +
 				ChatColor.YELLOW + ChatColor.BOLD + "i" +
 				ChatColor.GOLD + ChatColor.BOLD + ") " +
 				ChatColor.GREEN + ChatColor.BOLD + "Set Item Frame to ";
 		
 		if(!p.isSneaking()) return;
-		if(e.getRightClicked().getType().equals(EntityType.ITEM_FRAME)) {
-			if(item.isVisible() == true) {
+		if(e.getRightClicked().getType().equals(EntityType.ITEM_FRAME)||
+				e.getRightClicked().getType().equals(EntityType.GLOW_ITEM_FRAME)) {
+			if(item.isVisible() == true || gitem.isVisible() == true) {
 				item.setVisible(false);
+				gitem.setVisible(false);
+				
 				p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(msg + "Hide"));
 			}
 					else {
 						item.setVisible(true);
+						gitem.setVisible(true);
 						p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(msg + "Show"));
 			}
 			
