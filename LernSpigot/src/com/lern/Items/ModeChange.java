8573c265f92e93449a1de5fcaa5f1ec83176c792
BuildTools <unconfigured@null.spigotmc.org>
2022-01-22 14:09:51 +0700

    fix mode chang stick

--- LernSpigot/src/com/lern/Items/ModeChange.java
+++ LernSpigot/src/com/lern/Items/ModeChange.java
@@ -10,14 +10,38 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import com.lern.Main;
 
-public class ModeChange implements CommandExecutor{
+public class ModeChange implements CommandExecutor, Listener{
 	
+	private static ItemStack createmodewand() {
+	     ItemStack item = new ItemStack(Material.STICK);
+         ItemMeta meta = item.getItemMeta();
+ 
+         meta.setDisplayName("§5Chage Mode Stick");
+         List<String> lore = new ArrayList<>();
+         lore.add("§bRight Cick For Change Game Mode");
+         lore.add("§bCreative => Survival");
+         lore.add("§b Or");
+         lore.add("§bSurvival => Creative");
+         
+         meta.setLore(lore);
+         meta.addEnchant(Enchantment.FIRE_ASPECT, 1, false);
+         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+         
+         item.setItemMeta(meta);
+         
+         ItemStack modewand = (ItemStack) item;
+		
+		return modewand;
+		
+	}
+	 
 	@Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(!(sender instanceof Player)) {
@@ -25,24 +49,8 @@ public class ModeChange implements CommandExecutor{
         }
         if(label.equalsIgnoreCase("wand")) {
             Player p = (Player) sender;
-            ItemStack item = new ItemStack(Material.STICK);
-            ItemMeta meta = item.getItemMeta();
-    
-            meta.setDisplayName("§5Chage Mode Stick");
-            List<String> lore = new ArrayList<>();
-            lore.add("§bRight Cick For Change Game Mode");
-            lore.add("§bCreative => Survival");
-            lore.add("§b Or");
-            lore.add("§bSurvival => Creative");
-            
-            meta.setLore(lore);
-            meta.addEnchant(Enchantment.FIRE_ASPECT, 1, false);
-            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-            
-            item.setItemMeta(meta);
-            
-            ItemStack modewand = (ItemStack) item;
-            p.getInventory().addItem(modewand);
+       
+            p.getInventory().addItem(createmodewand());
             
             return true;       
         }
