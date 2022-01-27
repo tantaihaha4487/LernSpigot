d5fe8d380785e56e843e0d9a72735ca0c689fc21
tantaihaha <tantaiiottwo@gamil.com>
2022-01-27 21:55:28 +0700

    added SendAction bar api

--- LernSpigot/src/com/lern/commands/ReCommand.java
+++ LernSpigot/src/com/lern/commands/ReCommand.java
@@ -4,6 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.lern.utils.sendMessage;
 
 import net.md_5.bungee.api.ChatColor;
 
@@ -12,6 +15,7 @@ public class ReCommand implements CommandExecutor{
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player p = (Player) sender;
 		if(cmd.getName().equalsIgnoreCase("r")) {
 			Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "reload confirm");
 			sender.sendMessage(ChatColor.GOLD + ChatColor.BOLD.toString() + "Reload Complete!!");
