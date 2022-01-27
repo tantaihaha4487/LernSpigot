724f1e1495446b1e1731dffc11cdcd54fe81ee7a
TantaiHaha <78604116+tantaihaha4487@users.noreply.github.com>
2022-01-27 13:18:09 +0700

    Text change

--- LernSpigot/src/com/lern/commands/ReCommand.java
+++ LernSpigot/src/com/lern/commands/ReCommand.java
@@ -14,7 +14,35 @@ public class ReCommand implements CommandExecutor{
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("r")) {
 			Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "reload confirm");
-			sender.sendMessage(ChatColor.GOLD + "Wait..");
+			sender.sendMessage(ChatColor.GOLD + "Waipackage com.lern.commands;
+
+import org.bukkit.Bukkit;
+
+import org.bukkit.command.Command;
+
+import org.bukkit.command.CommandExecutor;
+
+import org.bukkit.command.CommandSender;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class ReCommand implements CommandExecutor{
+
+	@Overridet..");	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		if(cmd.getName().equalsIgnoreCase("r")) {
+
+			Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "reload confirm");
+
+			sender.sendMessage(ChatColor.GOLD + ChatColor.BOLD + "Reload Complete!");
+
+		}
+
+		return true;
+
+	}
+
+}
 		}
 		return true;
 	}
